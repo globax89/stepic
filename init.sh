98edcb478619e64f469a0de74b339beb2b338233
globax <globax89@gmail.com>
2016-11-19 01:01:49 +0300

    edit nginx conf and init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+sudo unlink /etc/nginx/sites-enabled/default
 ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test

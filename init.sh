8c94a4f34b396627f1e46d1f8c0ed753f2f7a650
globax <globax89@gmail.com>
2016-11-19 00:33:13 +0300

    all files for project

---
+++ init.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
+ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
+sudo /etc/init.d/mysql start

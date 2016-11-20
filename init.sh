9f6a8f290ed1fa36cbf6a79406d8010e64a779fd
globax <globax89@gmail.com>
2016-11-20 20:35:05 +0300

    add wsgi

--- init.sh
+++ init.sh
@@ -2,6 +2,7 @@
 sudo unlink /etc/nginx/sites-enabled/default
 ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+ln -s /home/box/web/hello.py /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/mysql start
+sudo gunicorn -c /home/box/web/etc/hello.py hello:app
